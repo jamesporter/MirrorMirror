a2c287bbf989f3e519d75547c5201801a5950a5b
James Porter <james@amimetic.co.uk>
2015-05-17 14:27:23 +0100

    Inverse tree pattern and some ugly hacks to make it partly work.

--- src/js/parallax-paper.js
+++ src/js/parallax-paper.js
@@ -1,4 +1,6 @@
 var paths = [];
+var rasters = [];
+
 var N = 50;
 
 view.size
@@ -13,6 +15,20 @@ for(var i=0; i<N; i++){
     tree.strokeColor = 'white';
     tree.strokeWidth = 20/ (i+1);
     paths.push(tree);
+
+
+    if( i % 10 == 0){
+
+        var leaf = new Raster({
+          source: "square0" + (i/10 + 1),
+          position: [view.size.width * Math.random(), view.size.width * Math.random()]
+        }).scale((Math.floor(Math.random() * 3) + 2) / 10) ;
+
+        leaf.origPos = leaf.position;
+
+      rasters.push(leaf);
+    }
+
 }
 
 var pos;
@@ -25,6 +41,11 @@ function onFrame(){
 
             //tree.position.y = tree.origPos.x + 0.001 * (tree.idNum + 1) * (view.center.y - faceY);
 
+        }
+
+        for(var idx in rasters){
+          var leaf = rasters[idx];
+          leaf.position.x = leaf.origPos.x + 0.1 * (view.center.x - faceX);
         }
     }
 }
