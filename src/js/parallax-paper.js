4f2d437f34083e311959560e40b94240d7325191
James Porter <james@amimetic.co.uk>
2015-05-17 13:07:15 +0100

    hide images, move single raster with head position.

--- src/js/parallax-paper.js
+++ src/js/parallax-paper.js
@@ -1,19 +1,8 @@
-new Raster({
+var sq = new Raster({
   source: 'square01',
   position: view.center
 });
 
-var children = project.activeLayer.children;
 function onFrame(event) {
-
-
-  for(var idx in children){
-    var child = children(idx);
-
-    idx.position
-
-
-  }
-
-
+  sq.position = new Point(faceX, faceY);
 }
